8bb67ada8c111f0debfd21f0a4965e343302f182
nnazo <jacobcurtis2266@gmail.com>
2021-08-01 11:10:26 -0500

    builder handle Option types

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,7 @@
 use darling::ToTokens;
 use proc_macro2::TokenStream;
-use quote::format_ident;
-use quote::quote;
-use syn::DataStruct;
-use syn::Ident;
-use syn::Type;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DataStruct, DeriveInput, GenericArgument, Ident, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -30,7 +26,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     out.to_token_stream().into()
 }
 
-fn get_struct_fields(r#struct: &DataStruct) -> Vec<(&Ident, &Type)> {
+fn get_struct_fields(r#struct: &DataStruct) -> Vec<(&Ident, &Type, Option<&Type>)> {
     match &r#struct.fields {
         syn::Fields::Named(fields) => fields
             .named
@@ -38,7 +34,7 @@ fn get_struct_fields(r#struct: &DataStruct) -> Vec<(&Ident, &Type)> {
             .map(|field| {
                 let ident = field.ident.as_ref().expect("Named");
                 let ty = &field.ty;
-                (ident, ty)
+                (ident, ty, option_inner_type(ty))
             })
             .collect::<Vec<_>>(),
         _ => unimplemented!(),
@@ -48,14 +44,22 @@ fn get_struct_fields(r#struct: &DataStruct) -> Vec<(&Ident, &Type)> {
 fn generate_builder_struct(struct_name: &Ident, r#struct: &DataStruct) -> (Ident, TokenStream) {
     let builder_name = format_ident!("{}Builder", struct_name);
 
-    let fields = transform_struct_fields(
-        r#struct,
-        |(ident, ty)| quote! { #ident: ::std::option::Option<#ty> },
-    );
+    let fields = transform_struct_fields(r#struct, |(ident, ty, opt)| {
+        let ty = match opt {
+            Some(inner_ty) => inner_ty.to_token_stream(),
+            _ => ty.to_token_stream(),
+        };
+        quote! { #ident: ::std::option::Option<#ty> }
+    });
 
     let setter_fns = transform_struct_fields_map(
         r#struct,
-        |(field, ty)| {
+        |(field, ty, opt)| {
+            let ty = match opt {
+                Some(inner_ty) => inner_ty.to_token_stream(),
+                None => ty.to_token_stream(),
+            };
+
             quote! {
                 pub fn #field (&mut self, #field: #ty) -> &mut Self {
                     self.#field = ::std::option::Option::Some(#field);
@@ -88,7 +92,7 @@ fn generate_builder_fn(
     builder_name: &Ident,
     r#struct: &DataStruct,
 ) -> TokenStream {
-    let fields = transform_struct_fields(r#struct, |(ident, _ty)| quote! { #ident: None });
+    let fields = transform_struct_fields(r#struct, |(ident, _ty, _opt)| quote! { #ident: None });
 
     quote! {
         impl #struct_name {
@@ -101,17 +105,53 @@ fn generate_builder_fn(
     }
 }
 
-fn generate_build_fn(struct_name: &Ident, r#struct: &DataStruct) -> TokenStream {
-    let fields = transform_struct_fields(r#struct, |(ident, _ty)| {
-        quote! {
-            #ident: self.#ident.clone().map_or_else(
-                || {
-                    let e: ::std::boxed::Box<dyn ::std::error::Error> = format!("Field not present").into();
-                    ::std::result::Result::Err(e)
-                },
-                |field| ::std::result::Result::Ok(field),
-            )?
+fn option_inner_type(ty: &Type) -> Option<&Type> {
+    match ty {
+        Type::Path(type_path) => {
+            let path_segment = type_path.path.segments.first();
+            match path_segment {
+                Some(path_segment) => {
+                    if path_segment.ident == "Option" {
+                        match &path_segment.arguments {
+                            syn::PathArguments::AngleBracketed(args) => {
+                                let arg = args.args.first();
+                                match arg {
+                                    Some(GenericArgument::Type(ty)) => Some(ty),
+                                    _ => None,
+                                }
+                            }
+                            _ => None,
+                        }
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            }
         }
+        _ => None,
+    }
+}
+
+fn generate_build_fn(struct_name: &Ident, r#struct: &DataStruct) -> TokenStream {
+    let fields = transform_struct_fields(r#struct, |(ident, _ty, opt)| {
+        let mut field = quote! {
+            #ident: self.#ident.clone()
+        };
+
+        if opt.is_none() {
+            field = quote! {
+                #field.map_or_else(
+                    || {
+                        let e: ::std::boxed::Box<dyn ::std::error::Error> = format!("Field not present").into();
+                        ::std::result::Result::Err(e)
+                    },
+                    |field| ::std::result::Result::Ok(field),
+                )?
+            }
+        }
+
+        field
     });
 
     quote! {
@@ -125,7 +165,7 @@ fn generate_build_fn(struct_name: &Ident, r#struct: &DataStruct) -> TokenStream 
 
 fn transform_struct_fields_map<F, G>(r#struct: &DataStruct, f: F, g: G) -> TokenStream
 where
-    F: FnMut((&Ident, &Type)) -> TokenStream,
+    F: FnMut((&Ident, &Type, Option<&Type>)) -> TokenStream,
     G: FnOnce(Vec<TokenStream>) -> TokenStream,
 {
     let fields = get_struct_fields(r#struct)
@@ -137,7 +177,7 @@ where
 
 fn transform_struct_fields<F>(r#struct: &DataStruct, f: F) -> TokenStream
 where
-    F: FnMut((&Ident, &Type)) -> TokenStream,
+    F: FnMut((&Ident, &Type, Option<&Type>)) -> TokenStream,
 {
     transform_struct_fields_map(r#struct, f, |fields| quote! { #( #fields ),* })
 }
